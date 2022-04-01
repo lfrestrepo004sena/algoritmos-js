af1981c9092408782075ea13063707903faa820d
lfrestrepo004sena <70179712+lfrestrepo004sena@users.noreply.github.com>
2022-04-01 09:03:35 -0500

    Proyecto Inicial

---
+++ Js/main.js
@@ -0,0 +1,12 @@
+function suma(){
+    var a;
+    var b;
+    var suma;
+    
+    a = parseInt (prompt("Por favor ingrese el primer valor"));
+    b = parseInt(prompt ("Por favor ingrese el segundo valor"));
+    suma = a + b;
+    
+    alert("El resultado de la suma es : " + suma);
+
+}
